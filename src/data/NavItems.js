6fd1514e7a787c92c547cbe698e28b358800676c
Rifqi Rifaldi <rifkyrifaldy005@gmail.com>
2024-09-28 22:22:59 +0700

    [FIX] Navbar menu

--- src/data/NavItems.js
+++ src/data/NavItems.js
@@ -3,81 +3,43 @@ const navItems = [
     id: 1,
     name: "Home",
     href: "/",
-    subNavItems: [
-      {
-        id: 1,
-        name: "Home One",
-        href: "/",
-      },
-      {
-        id: 2,
-        name: "Home Two",
-        href: "/home2",
-      },
-      {
-        id: 3,
-        name: "Home Three",
-        href: "/home3",
-      },
-      {
-        id: 4,
-        name: "Header Styles",
-        href: "/",
-        subItems: [
-          {
-            id: 1,
-            name: "Header One",
-            href: "",
-          },
-          { id: 2, name: "Header Two", href: "" },
-          { id: 3, name: "Header Three", href: "" },
-        ],
-      },
-    ],
+    subNavItems: [],
   },
   {
     id: 2,
-    name: "Pages",
-    href: "",
+    name: "Services",
+    href: "#",
     subNavItems: [
-      { id: 1, name: "About", href: "/about" },
-      { id: 2, name: "Volunteers", href: "/volunteers" },
-      { id: 3, name: "Gallery", href: "/gallery" },
-      { id: 4, name: "Become a Volunteer", href: "/become-volunteer" },
+      { id: 1, name: "Sukuk", href: "#" },
+      { id: 2, name: "Sharia Stock", href: "#" },
+      { id: 3, name: "Secondary Market", href: "#" },
     ],
   },
   {
     id: 3,
-    name: "Donations",
-    href: "/causes",
+    name: "About",
+    href: "#",
     subNavItems: [
-      { id: 1, name: "Causes", href: "/causes" },
-      { id: 2, name: "Causes Details", href: "/causes-details" },
+      { id: 1, name: "Invsma Team", href: "#" },
     ],
   },
   {
     id: 4,
-    name: "Events",
-    href: "/events",
+    name: "Information",
+    href: "#",
     subNavItems: [
-      { id: 1, name: "Events", href: "/events" },
-      { id: 2, name: "Event Details", href: "/event-details" },
+      { id: 1, name: "Publisher's Guide", href: "#" },
+      { id: 2, name: "Financire's Guide", href: "#" },
+      { id: 3, name: "Contacts", href: "#" },
     ],
   },
   {
     id: 5,
-    name: "News",
-    href: "/news",
+    name: "0812-8619-7142",
+    href: "#",
     subNavItems: [
-      { id: 1, name: "News", href: "/news" },
-      { id: 2, name: "News Details", href: "/news-details" },
+      
     ],
-  },
-  {
-    id: 6,
-    name: "Contact",
-    href: "/contact",
-    subNavItems: [],
   },
 ];
 
