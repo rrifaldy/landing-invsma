7c7f10de640b5eb5afbc66fe878b24b5b0181cc8
Rifqi Rifaldi <rifkyrifaldy005@gmail.com>
2024-09-29 02:04:35 +0700

    [ADD] Pasar sekunder page

---
+++ src/data/marketPage.js
@@ -0,0 +1,29 @@
+
+const marketPage = {
+    title: "Investasi dalam Pasar Sekunder",
+
+    images: [
+        {
+            src: "https://images.unsplash.com/photo-1487611459768-bd414656ea10?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHN0b2NrfGVufDB8MHwwfHx8MA%3D%3D"
+        },
+        {
+            src: "https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fHN0b2NrfGVufDB8MHwwfHx8MA%3D%3D"
+        },
+    ],
+    texts: [
+        "Pasar sekunder adalah tempat di mana saham dan sekuritas lainnya diperdagangkan setelah diterbitkan di pasar primer. Ini memberikan investor kesempatan untuk membeli dan menjual saham yang telah ada, menawarkan likuiditas dan akses ke berbagai pilihan investasi.",
+
+        "Investasi di pasar sekunder memungkinkan investor untuk berpartisipasi dalam fluktuasi harga saham, memberikan peluang untuk keuntungan jangka pendek dan strategi investasi yang lebih fleksibel. Dengan adanya pasar sekunder, investor dapat dengan mudah mengelola portofolio mereka dan bereaksi terhadap perubahan pasar."
+    ],
+
+    summaryText: "Pasar sekunder memberikan likuiditas yang penting dalam dunia investasi, memungkinkan perdagangan saham dan sekuritas secara efisien. Investor dapat dengan mudah membeli dan menjual investasi mereka, meningkatkan fleksibilitas dan strategi investasi.",
+    summaryList: [
+        "Likuiditas tinggi",
+        "Akses ke berbagai sekuritas",
+        "Keuntungan jangka pendek ",
+        "Fleksibilitas portofolio ",
+        "Reaksi cepat perubahan pasar",
+    ],
+};
+
+export default marketPage;
